932827290dc8422ad637c11b3cdbf3150f0625c2
Taylor Beck <taylor.beck@engageft.com>
2019-08-18 17:25:51 -0700

    Load collections via Firebase, add loading spinner HOC, fix button styles

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,18 +12,23 @@ const config = {
   appId: "1:1050738988494:web:df52f510b4740cc1"
 }
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   // User not logged in.
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const collectionRef = firestore.collection('users');
 
   const snapShot = await userRef.get();
+  const collectionSnapshot = await collectionRef.get();
 
   if(!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
+    // Create new user
     try {
       await userRef.set({
         displayName,
@@ -39,7 +44,37 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 }
 
-firebase.initializeApp(config);
+// Importing new collections
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);  
+
+  // Batch Firestore calls
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
